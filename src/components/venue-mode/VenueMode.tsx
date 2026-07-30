ecaed1c31f244f463f7778b303569a510ded8443
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-07-30 10:34:47 -0400

    Add files via upload

---
+++ src/components/venue-mode/VenueMode.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VenueMode: React.FC = () => {
+  return (
+    <div>
+      <h2>Venue Mode</h2>
+      <p>Multi-screen venue controls will appear here.</p>
+    </div>
+  );
+};
+
+export default VenueMode;
