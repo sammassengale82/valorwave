f1f261dff30d91389d6f4d1a0917d6397a2bc2b0
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-07-30 10:30:19 -0400

    Add files via upload

---
+++ src/components/show-summary/ShowSummary.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShowSummary: React.FC = () => {
+  return (
+    <div>
+      <h2>Show Summary</h2>
+      <p>Analytics and show history will appear here.</p>
+    </div>
+  );
+};
+
+export default ShowSummary;
